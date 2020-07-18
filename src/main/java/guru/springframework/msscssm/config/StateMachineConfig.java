71bbba7d8202e251a3174d9e3e23833ab0313cc7
niro1982 <n.edinger1982@gmail.com>
2020-07-18 14:28:57 +0300

    implementing notification actions as separate components

--- src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -2,10 +2,9 @@ package guru.springframework.msscssm.config;
 
 import guru.springframework.msscssm.domain.PaymentEvent;
 import guru.springframework.msscssm.domain.PaymentState;
-import guru.springframework.msscssm.services.PaymentServiceImpl;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
@@ -17,15 +16,27 @@ import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
 import java.util.EnumSet;
-import java.util.Random;
 
 /**
  * Created by jt on 2019-07-23.
  */
 @Slf4j
+@RequiredArgsConstructor
 @EnableStateMachineFactory
 @Configuration
 public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    //doing Autowire by name and not by type cause all of my instances are Actions.
+    //spring will try to wire by type first and when it cant it will look for spring beans (components) with the same name here with lower case.
+    //if i give the instances a name that is different from the class name, i will need to initialize it in the constructor and to use @qualifier
+    //to let spring know exactly which class to autowire since they are all instances of Action class.
+    private final Action<PaymentState, PaymentEvent> authAction;
+    private final Action<PaymentState, PaymentEvent> preAuthAction;
+    private final Guard<PaymentState, PaymentEvent> paymentIdGuard;
+    private final Action<PaymentState, PaymentEvent> authApprovedAction;
+    private final Action<PaymentState, PaymentEvent> authDeclinedAction;
+    private final Action<PaymentState, PaymentEvent> preAuthApprovedAction;
+    private final Action<PaymentState, PaymentEvent> preAuthDeclinedAction;
 
     @Override
     public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
@@ -40,19 +51,19 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
     @Override
     public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
         transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
-                    .action(preAuthAction()).guard(paymentIdGuard())
+                    .action(preAuthAction).guard(paymentIdGuard)
                 .and()
-                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
+                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED).action(preAuthApprovedAction)
                 .and()
-                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED)
+                .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED).action(preAuthDeclinedAction)
                 //preauth to auth
                 .and()
                 .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.PRE_AUTH).event(PaymentEvent.AUTHORIZE)
-                    .action(authAction())
+                    .action(authAction)
                 .and()
-                .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH).event(PaymentEvent.AUTH_APPROVED)
+                .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH).event(PaymentEvent.AUTH_APPROVED).action(authApprovedAction)
                 .and()
-                .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH_ERROR).event(PaymentEvent.AUTH_DECLINED);
+                .withExternal().source(PaymentState.PRE_AUTH).target(PaymentState.AUTH_ERROR).event(PaymentEvent.AUTH_DECLINED).action(authDeclinedAction);
     }
 
     @Override
@@ -68,13 +79,13 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
                 .listener(adapter);
     }
 
-    public Guard<PaymentState, PaymentEvent> paymentIdGuard(){
+/*    public Guard<PaymentState, PaymentEvent> paymentIdGuard(){
         return context -> {
             return context.getMessageHeader(PaymentServiceImpl.PAYMENT_ID_HEADER) != null;
         };
-    }
+    }*/
 
-    public Action<PaymentState, PaymentEvent> preAuthAction(){
+/*    public Action<PaymentState, PaymentEvent> preAuthAction(){
         return context -> {
             System.out.println("PreAuth was called!!!");
 
@@ -91,9 +102,9 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
                         .build());
             }
         };
-    }
+    }*/
 
-    public Action<PaymentState, PaymentEvent> authAction(){
+    /*public Action<PaymentState, PaymentEvent> authAction(){
         return context -> {
             System.out.println("Auth was called!!!");
 
@@ -110,5 +121,5 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
                         .build());
             }
         };
-    }
+    }*/
 }
